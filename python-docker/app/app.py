2518765b415f9eaa185f789a9a681fcf9c904247
lexxxaners <86820729+lexxxaners@users.noreply.github.com>
2024-01-27 10:09:32 +0400

    Update app.py

--- python-docker/app/app.py
+++ python-docker/app/app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def hello_world():
     try:
         conn = psycopg2.connect(host="postgresql_container",port="5432",database="postgres",user="postgres",password="postgres")
-        return 'Hello, Dockereeeee!'
+        return 'Hello, Docker!'
     except:
         return 'No connect!'
